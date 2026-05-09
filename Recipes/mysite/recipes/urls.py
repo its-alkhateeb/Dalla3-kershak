a0ce3461a14ddbff9746d755507ee7674029cb4b
Yassin <yassin.mail.an@gmail.com>
2026-05-09 06:23:22 +0300

    adding Recipe_details page

--- Recipes/mysite/recipes/urls.py
+++ Recipes/mysite/recipes/urls.py
@@ -7,8 +7,16 @@ urlpatterns = [
     path('', home, name='home'),
     path('recipes/', views.recipes_list, name='recipes_list'),
     path('favourites/', views.favourites, name='favorites'),
+<<<<<<< Updated upstream
     path('recipes/add/', views.add_recipe, name='add_recipe'),
    path('recipes/edit/<int:pk>/', views.edit_recipe, name='edit_recipe'),
    path('recipes/<int:pk>/delete/', views.delete_recipe, name='delete_recipe'),
      path('recipes/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
 ]
+=======
+    #path('recipes/add/', views.add_recipe, name='add_recipe'),
+   #path('recipes/edit/<int:pk>/', views.edit_recipe, name='edit_recipe'),
+   #path('recipes/<int:pk>/delete/', views.delete_recipe, name='delete_recipe'),
+   path('recipes/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+]
+>>>>>>> Stashed changes
